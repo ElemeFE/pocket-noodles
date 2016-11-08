db4706c43ab826927aa1f84e7784dbfb2d3f008e
YanagiEiichi <admin@web-tinker.com>
2016-11-08 14:39:55 +0800

    Set 999 status code as net error

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+/**/ void function(scope) {
+/**/
+/**/   // CommonJS
+/**/   if (typeof module === 'object' && !!module.exports) return scope(function(name, dependencies, factory) {
+/**/     if (typeof name !== 'string') factory = dependencies, dependencies = name, name = null;
+/**/     if (!(dependencies instanceof Array)) factory = dependencies, dependencies = [];
+/**/     var args;
+/**/     args = [ require('FCeptor'), require('XCeptor') ];
+/**/     module.exports = factory.apply(module.exports, args) || module.exports;
+/**/   });
+/**/
+/**/   // AMD, wrap a 'String' to avoid warn of fucking webpack
+/**/   if (String(typeof define) === 'function' && !!define.amd) return scope(define);
+/**/
+/**/   // Global
+/**/   scope(function(name, dependencies, factory) {
+/**/     if (typeof name !== 'string') factory = dependencies, dependencies = name, name = null;
+/**/     if (!(dependencies instanceof Array)) factory = dependencies, dependencies = [];
+/**/     var exports = {};
+/**/     var args = [];
+/**/     for (var i = 0; i < dependencies.length; i++) args[i] = window[dependencies[i]];
+/**/     exports = factory.apply(exports, args) || exports;
+/**/     if (name) {
+/**/       /**/ try { /* Fuck IE8- */
+/**/       /**/   if (typeof execScript === 'object') execScript('var ' + name);
+/**/       /**/ } catch(error) {}
+/**/       window[name] = exports;
+/**/     }
+/**/   });
+/**/
+/**/ }(function(define) {
+
 define([ 'FCeptor', 'XCeptor' ], function(FCeptor, XCeptor) {
 
   var MAX_SIZE = 2 * 1024 * 1024;
@@ -131,3 +163,5 @@ define([ 'FCeptor', 'XCeptor' ], function(FCeptor, XCeptor) {
   }
 
 });
+
+/**/ });
